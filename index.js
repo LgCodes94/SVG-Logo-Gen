1cc1fe8d000d3e4d91200612976e25a9cc408241
unknown <lgcodes94@gmail.com>
2023-11-25 22:36:32 -0600

    fixed bugs

--- index.js
+++ index.js
@@ -1,35 +1,81 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./lib/shapes');
-const { getUserInput, getUserColor, getUserShape } = require('./lib/userInput');
 
-async function generateLogo() {
-  const text = await getUserText();
-  const textColor = await getUserColor('text');
-  const shapeType = await getUserShape();
-  const shapeColor = await getUserColor(`${shapeType} shape`);
+function generateLogo() {
+  inquirer
+    .prompt([
 
-  let shape;
-  switch (userInput.shape) {
-    case 'triangle':
-      shape = new Triangle();
+      {
+        type: "input",
+        name: "text",
+        message: "Enter 3 characters to display in your SVG logo",
+      },
+      {
+        type: "input",
+        name: "TextColor",
+        message: "Enter Color keyword or hexadecimal number",
+      },
+      {
+        type: "input",
+        name: "ShapeColor",
+        message: "Enter shape color keyword or hexadecimal number",
+      },
+      {
+        type: "list",
+        name: "image",
+        message: "Choose shape you like",
+        choice: ["Circle", "Square", "Triangle"],
+      }
+    ])
+    .then((response) => {
+      if (response.text.length > 3) {
+        console.log('text cannot be more than 3 chars');
+        generateLogo();
+      } else {
+        writeToFile('logo.svg', response);
+      }
+    });
+}
+
+
+const util = require('util');
+const writeFile = util.promisify(fs.writeFile);
+
+function writeToFile(fileName, responses) {
+  let svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svgString += '<g>';
+
+  let shapeChoice;
+  switch (responses.image) {
+    case 'Circle':
+      shapeChoice = new Circle();
       break;
-    case 'circle':
-      shape = new Circle();
+    case 'Square':
+      shapeChoice = new Square();
       break;
-    case 'square':
-      shape = new Square();
+    case 'Triangle':
+      shapeChoice = new Triangle();
       break;
     default:
-      console.error('Invalid shape');
-      return;
+      throw new Error('Invalid shape choice');
   }
 
-  shape.setColor(userInput.shapeColor);
-  const svgContent = `<svg width="300" height="200">${shape.render()}</svg>`;
-  const filename = 'logo.svg';
+  shapeChoice.setColor(responses.ShapeColor);
 
-  fs.writeFileSync(filename, svgContent);
-  console.log('Generated logo.svg');
+  svgString += shapeChoice.render();
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${responses.TextColor}">${responses.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
+
+  try {
+    fs.writeFile(fileName, svgString)
+      .then(() => {
+        console.log("Generated logo.svg");
+      })
+  } catch (err) {
+    console.log(err);
+  };
 }
 
 generateLogo();
